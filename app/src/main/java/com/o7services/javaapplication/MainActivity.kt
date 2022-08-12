c86ca66f35f2425663c74988b3a1f0c6e0ac84d8
Amanpreet <69182131+amanpreeto7@users.noreply.github.com>
2022-08-12 11:14:36 +0530

    Shared Preferences and recycler view added

--- app/src/main/java/com/o7services/javaapplication/MainActivity.kt
+++ app/src/main/java/com/o7services/javaapplication/MainActivity.kt
@@ -5,27 +5,27 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.o7services.javaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var etName: EditText
-    lateinit var etPassword: EditText
-    lateinit var btnLogin: Button
+    lateinit var binding: ActivityMainBinding
+        var arrayList = arrayOf(1,2,3)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        etName = findViewById(R.id.etName)
-        etPassword = findViewById(R.id.etPassword)
-        btnLogin = findViewById(R.id.btnLogin)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        arrayList[0] = 5
 
-        btnLogin.setOnClickListener {
+        binding.btnLogin.setOnClickListener {
             System.out.println("Login Clicked")
-            var name = etName.text.toString()
-            var password = etPassword.text.toString()
+            var name = binding.etName.text.toString()
+            var password = binding.etPassword.text.toString()
             if(name.isNullOrEmpty() == true){
-                etName.error = resources.getString(R.string.enter_name)
-                etName.requestFocus()
+                binding.etName.error = resources.getString(R.string.enter_name)
+                binding.etName.requestFocus()
             }else if(password.isNullOrEmpty()) {
-                etPassword.error = resources.getString(R.string.login_password)
+                binding.etPassword.error = resources.getString(R.string.login_password)
             }else{
                 Toast.makeText(this, resources.getString(R.string.login_successfully), Toast.LENGTH_LONG)
             }
--- app/src/main/java/com/o7services/javaapplication/RegistrationActivity.kt
+++ app/src/main/java/com/o7services/javaapplication/RegistrationActivity.kt
@@ -2,8 +2,10 @@ package com.o7services.javaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.o7services.javaapplication.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
